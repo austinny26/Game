ab3388bf68c72289661856fdd2946b901fceb06d
austinny26 <austinzhou0215@gmail.com>
2025-04-29 10:55:29 -0400

    Added Coins, slime enemy, player death, map update

---
+++ scripts/slime.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+const SPEED = 60
+
+var direction = 1
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.x += SPEED * delta
