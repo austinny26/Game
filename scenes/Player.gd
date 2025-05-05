fda7fe1633ec29b020c222f63b525f95a473e5df
austinny26 <austinzhou0215@gmail.com>
2025-05-05 10:53:10 -0400

    Playing around with signals and trying to get a damage system in

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -6,7 +6,13 @@ const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var max_health = 100
+signal take_damage(int)
 
+func _process(delta):
+	if(max_health < 0):
+			get_tree().reload_current_scene() 
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -21,8 +27,17 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
+		if(direction == -1):
+			animated_sprite.flip_h = true
+		else:
+			animated_sprite.flip_h = false
 		velocity.x = direction * SPEED
+	
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+
+
--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,13 +1,15 @@
 extends Area2D
-
 @onready var timer = $Timer
-
+@onready var player = $"../Player"
 
 func _on_body_entered(body):
+	emit_signal("take_damage", 10)
 	timer.start()
 
 
 
 
-func _on_timer_timeout():
-	get_tree().reload_current_scene()
+
+
+func _on_player_take_damage(int):
+	pass # Replace with function body.
