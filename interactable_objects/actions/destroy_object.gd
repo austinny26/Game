7bb2bdf8965656066d308eb3fd0f06254fac4f86
austinny26 <austinzhou0215@gmail.com>
2025-06-04 10:50:24 -0400

    Delete interactable_objects directory

--- interactable_objects/actions/destroy_object.gd
+++
@@ -1,7 +0,0 @@
-extends InteractableObjectAction
-
-# A simple example of QuiverInteractableObjectAction that just instantly destroys the object
-
-func trigger(object: InteractableObject, character: Character):
-	object.queue_free()
-
